3b3d8a4ea4da0166127d5785a01e5d7a1a1da511
darkobits <joshua@frontlawn.net>
2019-11-27 23:02:10 -0800

    fix: Fix casing on file imprt.

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from 'client/App';
+import App from 'client/app';
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
